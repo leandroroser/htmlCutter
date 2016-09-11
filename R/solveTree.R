549ec32486ae1ae28582dc72e7cef823b23136aa
Leandro Roser <learoser@gmail.com>
2016-09-11 04:30:17 -0300

    Update solveTree.R

--- R/solveTree.R
+++ R/solveTree.R
@@ -1,6 +1,6 @@
 
 #' Find nodes and branches of an HTML tree in a nested set model representation.
-#' @description Find nodes and branchs of an HTML tree in a nested set model representation
+#' @description Find nodes and branches of an HTML tree in a nested set model representation
 #' @param a Representation of the tree as a linear vector nodes  numbers (nested set model)
 #' @author Leandro Roser \email{leandroroser@@gmail.com}
 #' @export
